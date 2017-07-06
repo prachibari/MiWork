42ee0198b47d4251721a8af5863871e61df9890a
prachibari07 <prachibari07@gmail.com>
2017-07-06 12:44:27 +0530

    Version 1.1 with ripple animation

--- app/src/main/java/com/example/prachi/miworkapp/NumbersActivity.java
+++ app/src/main/java/com/example/prachi/miworkapp/NumbersActivity.java
@@ -24,47 +24,12 @@ import static android.media.AudioManager.AUDIOFOCUS_LOSS_TRANSIENT;
 public class NumbersActivity extends AppCompatActivity {
 
 
-    private MediaPlayer mediaPlayer;
-    private AudioManager audioManager;
-
-    private MediaPlayer.OnCompletionListener onCompletionListener = new MediaPlayer.OnCompletionListener() {
-        @Override
-        public void onCompletion(MediaPlayer mp) {
-            releaseMediaPlayer();
-        }
-    };
-
-    AudioManager.OnAudioFocusChangeListener changeListener = new AudioManager.OnAudioFocusChangeListener()
-    {
-        @Override
-        public void onAudioFocusChange(int focusChange) {
-            if(focusChange==AudioManager.AUDIOFOCUS_LOSS_TRANSIENT ||
-                    focusChange== AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK)
-            {
-                //pause
-                mediaPlayer.pause();
-                mediaPlayer.seekTo(0);
-            }
-            else if(focusChange== AudioManager.AUDIOFOCUS_GAIN)
-            {
-                //resume playback
-                mediaPlayer.start();
-            }
-            else if(focusChange==AudioManager.AUDIOFOCUS_LOSS)
-            {
-                //stop playback
-                releaseMediaPlayer();
-            }
-        }
-    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_numbers);
+        setContentView(R.layout.activity_category);
 
-        audioManager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
-
-
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,new NumbersFragment()).commit();
 
        /* ArrayList<String> words = new ArrayList<String>();
         words.add("one");
@@ -100,54 +65,6 @@ public class NumbersActivity extends AppCompatActivity {
          GridView gridView = (GridView)findViewById(R.id.glist);
         gridView.setAdapter(numbersAdapter); */
 
-        final ArrayList<Word> words = new ArrayList<Word>();
-        words.add(new Word("one","ek",R.drawable.number_one,R.raw.number_one));
-        words.add(new Word("two","do",R.drawable.number_two,R.raw.number_two));
-        words.add(new Word("three","teen",R.drawable.number_three,R.raw.number_three));
-        words.add(new Word("four","char",R.drawable.number_four,R.raw.number_four));
-        words.add(new Word("five","panch",R.drawable.number_five,R.raw.number_five));
-        words.add(new Word("six","che",R.drawable.number_six,R.raw.number_six));
-        words.add(new Word("seven","sath",R.drawable.number_seven,R.raw.number_seven));
-        words.add(new Word("eight","ath",R.drawable.number_eight,R.raw.number_eight));
-        words.add(new Word("nine","nav",R.drawable.number_nine,R.raw.number_nine));
-        words.add(new Word("ten","daha",R.drawable.number_ten,R.raw.number_ten));
 
-        WordAdapter wordAdapter = new WordAdapter(this,words,R.color.category_numbers);
-        ListView listView = (ListView)findViewById(R.id.list);
-        listView.setAdapter(wordAdapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Word word = words.get(position);
-                releaseMediaPlayer();
-
-                int result= audioManager.requestAudioFocus(changeListener,AudioManager.STREAM_MUSIC,AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
-
-                if(result==AudioManager.AUDIOFOCUS_REQUEST_GRANTED)
-                {
-                    //audioManager.registerMediaButtonEventReceiver(RemoteControlReceiver);
-                    mediaPlayer = MediaPlayer.create(NumbersActivity.this,word.getAudioResourceId());
-                    mediaPlayer.start();
-                    mediaPlayer.setOnCompletionListener(onCompletionListener);
-                }
-
-            }
-        });
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        releaseMediaPlayer();
-    }
-
-    private void releaseMediaPlayer()
-    {
-        if(mediaPlayer != null)
-        {
-            mediaPlayer.release();
-            mediaPlayer = null;
-            audioManager.abandonAudioFocus(changeListener);
-        }
     }
 }
